74aead83549519f7127e3159b7e9c728186c6977
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-05-01 19:28:52 +0300

    credit card

--- src/pages/Lib/Lib.js
+++ src/pages/Lib/Lib.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactModal from "../../components/Lib/Modal/ReactModal";
 import Tooltip from "../../components/Lib/Tooltip/Tooltip";
 import ColorPicker from "../../components/Lib/ColorPicker/ColorPicker";
+import CreditCards from "../../components/Lib/CreditCards/CreditCards";
 
 const Lib = () => {
   return (
@@ -10,6 +11,7 @@ const Lib = () => {
       <ReactModal />
       <Tooltip />
       <ColorPicker />
+      <CreditCards />
     </div>
   );
 };
